a2bf86a33634a9c49a39365c9fc66ac043775aeb
eaejaum <jpedroalmeida179@gmail.com>
2025-01-08 10:57:18 -0300

    installing tailwind and shadcn

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
-    <h1>Registration</h1>
-  )
+    <>
+      <h1>Registration</h1>
+      <Button>Teste</Button>
+    </>
+  );
 }
